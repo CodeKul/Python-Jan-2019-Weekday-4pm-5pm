0d6b7265dd1d453695c121ed305f256376903dc1
varunkudalkar <varun.kudalkar@gmail.com>
2019-01-10 17:43:02 +0530

    Demos

---
+++ Operators.py
@@ -0,0 +1,67 @@
+# Assignment
+"""
+=
+"""
+
+a = 10
+b = 20
+
+# Arithmetic
+"""
++
+-
+*
+/
+%
+**
+"""
+c = 789 ** 2
+
+# Compound Assignment
+"""
++=
+-=
+*=
+/=
+%=
+**=
+"""
+
+a -= b  # a = a + b
+
+# Comparision
+"""
+<
+>
+==
+<=
+>=
+!=
+"""
+
+a = 20
+
+res = a <= b
+
+
+# Logical
+"""
+and
+or
+not
+
+p   q   and or  not(p)
+t   t   t   t   f
+t   f   f   t   f
+f   t   f   t   t
+f   f   f   f   t
+"""
+
+res = not ((a == b) or (a != 20))
+
+print(res)
+
+a += 1
+
+print(a)
+
---
+++ Variables.py
@@ -0,0 +1,15 @@
+var = "10.20"
+
+var = 10
+
+print(var)
+
+myBool = True
+
+a = None
+
+MY_CONST = 20
+
+MY_CONST = 100
+
+print(MY_CONST)
