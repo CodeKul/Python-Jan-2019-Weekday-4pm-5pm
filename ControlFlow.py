efb6447df9a5def7bcac25200ea62e6a1705f0c6
varunkudalkar <varun.kudalkar@gmail.com>
2019-01-11 17:06:21 +0530

    controlflow

---
+++ ControlFlow.py
@@ -0,0 +1,57 @@
+'''
+if condition:
+    statement1
+    statement2
+else:
+    statement3
+    statement4
+'''
+
+a = 10
+b = 560
+
+if a < b:
+    print('value of a is less than value of b')
+elif a == b:
+    print('value of a and b are equal')
+else:
+    print('value of a is greater than value of b')
+
+if a == 10 and b == 20:
+    print('Best values')
+else:
+    print('Bad values')
+
+
+# Loops
+
+'''
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+Codekul
+
+init
+while condition:
+    statemnets
+    incr/decr
+
+'''
+
+
+i = 10
+while i > 0:
+    print('Codekul')
+    i -= 1
+
+
+'''
+for item in collecction:
+    statements
+'''
